f5fe25bf0159a8d27900fea822dd654bf8ac7588
italoribeiroc <italovianelli59@hotmail.com>
2026-07-20 23:25:49 +0900

    Fix Vercel deployment routing

--- eyelearn/api/views.py
+++ eyelearn/api/views.py
@@ -2,6 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view(['GET'])
+def home(request):
+    return Response({
+        'message': 'API is running',
+        'hello_url': '/api/hello/Italo/'
+    })
+
+@api_view(['GET'])
 def hello_user(request, username):
     return Response({
         'message': f'Hello, {username}!'
