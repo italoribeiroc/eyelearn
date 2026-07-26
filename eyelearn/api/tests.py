0e6a03f50c2692d71edc89921709c6bef3bb5448
italoribeiroc <italovianelli59@hotmail.com>
2026-07-26 19:31:54 +0900

    Add GitHub Actions CI/CD

--- eyelearn/api/tests.py
+++ eyelearn/api/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class ApiEndpointTests(SimpleTestCase):
+    def test_home_endpoint(self):
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['message'], 'API is running')
+
+    def test_hello_endpoint(self):
+        response = self.client.get('/api/hello/Italo/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'message': 'Hello, Italo!'})
